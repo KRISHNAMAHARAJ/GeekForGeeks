bb0f1631ae959a9ee1c28358f20f9924f7660922
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-11-29 23:22:01 +0530

    Update armstrong-numbers.cpp

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -10,7 +10,7 @@ class Solution {
   public:
     bool armstrongNumber(int n) {
         // code here
-        int k = (int)(log10(n)+1); 
+        int k = (int)(log10(n)+1);  ///// int k = to_string(n).length();
 	    int result =0;
 	    int temp = n;
     	while(temp>0) {
