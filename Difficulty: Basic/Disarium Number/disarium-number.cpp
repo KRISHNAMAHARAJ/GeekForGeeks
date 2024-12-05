bda57e5fc85ce1d79f939f06515775155fd8b9ab
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-06 03:24:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Disarium Number/disarium-number.cpp
+++ Difficulty: Basic/Disarium Number/disarium-number.cpp
@@ -7,7 +7,31 @@ class Solution {
   public:
     int isDisarium(int N) {
         // code here
-        string s = to_string(N);
+       int count =0;
+       int temp = N;
+       while(temp>0){
+           count++;
+           temp = temp/10;
+       }
+       temp = N;
+       int sum=0;
+       for(int i=count; i>0; i--){
+           int digit = temp %10;
+           sum = sum + pow(digit, i);
+           temp /=10;
+       }
+       return (sum == N);
+    }   
+};
+
+
+
+
+/////we prefer this string method //////
+////////Using string method ////
+/*
+
+ string s = to_string(N);
         int sum=0;
         for(int i=0; i<s.length(); i++){
             int digit = (s[i]-48);
@@ -15,8 +39,7 @@ class Solution {
         }
         if(sum == N) return 1;
         else return 0;
-    }   
-};
+*/
 
 //{ Driver Code Starts.
 int main() {
