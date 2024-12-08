1dd00a8663bcb4d56b1a09151721f17575516907
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-08 22:43:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Happy Number/happy-number.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    bool isHappy(int n) {
+        int slow = getNextNumber(n);
+        int fast = getNextNumber(getNextNumber(n));
+
+        while (slow != fast) {
+            if (fast == 1) return true;
+            slow = getNextNumber(slow);
+            fast = getNextNumber(getNextNumber(fast));
+        }
+
+        return slow == 1;
+    }
+
+private:
+    int getNextNumber(int n) {
+        int output = 0;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            output += digit * digit;
+            n = n / 10;
+        }
+        
+        return output;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.isHappy(N) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0; 
+}
+// } Driver Code Ends
