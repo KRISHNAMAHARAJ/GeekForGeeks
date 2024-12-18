4a7dec72409923d5d93990c7eddf9bef9abebaa7
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-18 19:42:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -14,10 +14,38 @@ class Solution {
     // k : find kth smallest element and return using this function
     int kthSmallest(vector<int> &arr, int k) {
         // code here
+        
+        /// Using Counting Sort TC:- O(n + max_element) SC:- O(max_element)
+
+        int maxElement = *max_element(arr.begin(), arr.end());
+        vector<int> count(maxElement + 1, 0);
+    
+        for (int i = 0; i < arr.size(); i++) {
+            count[arr[i]]++;
+        }
+    
+        int cumulativeCount = 0;
+        for (int i = 1; i <= maxElement; i++) {
+            cumulativeCount += count[i];
+            if (cumulativeCount >= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+};
+
+
+
+//////Using sort function TC:- O(nlogn) and SC:- O(1)
+/*
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
         sort(arr.begin(), arr.end());
         return arr[k-1];
     }
-};
+*/
 
 //{ Driver Code Starts.
 
