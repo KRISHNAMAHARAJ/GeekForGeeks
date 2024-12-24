20694dd4fec728f9c83cef9435d2d518e67be691
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-24 21:56:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.cpp
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.cpp
@@ -13,7 +13,7 @@ class Solution {
         // code here
         
         int n = mat.size();
-        int m= mat[0].size();
+        int m= mat[0].size();           ///// TC:- O(n+m) SC:- O(1)
         
         int left = 0, top = m-1;
         
@@ -30,7 +30,35 @@ class Solution {
         }
         return false;
     }
+
 };
+
+
+//////// Using TC:- O(log(n*m)). SC:- O(1)
+
+/*
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+    int n = mat.size();
+    int m = mat[0].size();
+    int low = 0, high = n * m - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int midElement = mat[mid / m][mid % m]; // Map mid to 2D indices
+
+        if (midElement == x) {
+            return true;
+        } else if (midElement < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return false;
+}
+
+*/
 
 //{ Driver Code Starts.
 int main() {
