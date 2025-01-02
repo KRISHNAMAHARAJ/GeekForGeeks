a9556f22dbab323cb75b5e25954d01135654d37d
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-02 22:02:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Power of 2/power-of-2.cpp
+++ Difficulty: Easy/Power of 2/power-of-2.cpp
@@ -18,6 +18,20 @@ class Solution {
     }
 };
 
+
+
+/////// Using Iteration 
+/*
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false; 
+        while (n > 1) {
+            if (n % 2 != 0) return false; 
+            n /= 2;
+        }
+        return true;
+    }
+*/
+
 //{ Driver Code Starts.
 
 // Driver code
