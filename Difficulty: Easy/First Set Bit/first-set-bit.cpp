2604e041ae85bbb05e83edbad03424b93cac6447
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-20 12:44:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Set Bit/first-set-bit.cpp
+++ Difficulty: Easy/First Set Bit/first-set-bit.cpp
@@ -13,9 +13,6 @@ class Solution {
     // Function to find position of first set bit in the given number.
     int getFirstSetBit(int n) {
         // Your code here
-        if(n == 0){
-            return 0;
-        }
         
         int count = 1;
         while(n){
