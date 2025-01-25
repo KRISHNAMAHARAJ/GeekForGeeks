2bbe76fd2ce0185ea0a4c62c0fa762c14a9e79c1
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-25 21:40:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left most and right most index/left-most-and-right-most-index.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        // code here
+        int n = v.size();
+    long first = -1, last = -1;
+
+    // Binary search for the first occurrence
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (v[mid] == x) {
+            first = mid;
+            high = mid - 1;  // Move left to find earlier occurrence
+        } else if (v[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    // Binary search for the last occurrence
+    low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (v[mid] == x) {
+            last = mid;
+            low = mid + 1;  // Move right to find later occurrence
+        } else if (v[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (first == -1) {
+        return {-1, -1};  // Element not found
+    }
+    return {first, last};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, k;
+        vector<long long>v;
+        cin>>n;
+        for(long long i=0;i<n;i++)
+        {
+            cin>>k;
+            v.push_back(k);
+        }
+        long long x;
+        cin>>x;
+        Solution obj;
+        pair<long,long> pair = obj.indexes(v, x);
+        
+        if(pair.first==pair.second and pair.first==-1)
+        cout<< -1 <<endl;
+        else
+        cout<<pair.first<<" "<<pair.second<<endl;
+
+    
+cout << "~" << "\n";
+}
+	return 0;
+}
+
+// } Driver Code Ends
