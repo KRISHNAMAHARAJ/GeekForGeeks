026e2f2251be9ccbd3fd8d3b60285f9a757a6192
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-26 03:34:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
@@ -36,8 +36,55 @@ class Solution {
             }
         }
         return n+1;
+
     }
 };
+
+//Above method is optimal and best
+
+// test case:- [2,3,0,11,1,-7,3]  - this test case is good for making any logic
+
+
+///////3rd Method:-  Using Hash Set TC:- O(n) and SC:- O(n) here extra space is used.
+/*
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+        unordered_set<int> st;
+
+        for(int i=0; i<n; i++){
+            st.insert(nums[i]);
+        }
+        for(int i = 1; i<=n+1; i++){
+            if(st.find(i) == st.end()){
+                return i;
+            }
+        }
+        return n+1;    
+    }
+*/
+
+
+
+///////2nd Method:- Using Sorting  TC:- O(nlogn) and SC:- O(1) 
+/*
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+           
+        sort(nums.begin(), nums.end());
+        int cand = 1;
+        for(int i=0; i< n; i++){
+            if(nums[i] > cand){
+                return cand;
+            }
+            else if(nums[i] == cand){
+                cand++;
+            }
+        }
+        return cand;
+    }
+*/
 
 //{ Driver Code Starts.
 
