25a2a18638fa6e6e026053d519628d63c6701783
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-31 21:55:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -10,6 +10,41 @@ class Solution {
      
         int n = arr.size();
         
+        int left =0, right = n-1;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+            if(arr[mid] == key) return mid;
+            
+            if(arr[left] <= arr[mid]){          // Left sorted
+                if(arr[left] <= key && key <= arr[mid]){
+                    right = mid -1;
+                }
+                else {
+                    left = mid +1;
+                }
+            }
+            else {                              // Right Sorted
+                if(arr[mid] <= key && key <= arr[right]){
+                    left = mid +1;
+                }
+                else {
+                    right = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+////// Using Linear Search TC:- O(n) 
+/*
+    int search(vector<int>& arr, int key) {
+     
+        int n = arr.size();
         for(int i=0; i<n; i++){
             if(arr[i] == key){
                 return i;
@@ -18,7 +53,7 @@ class Solution {
         return -1;
         
     }
-};
+*/
 
 //{ Driver Code Starts.
 int main() {
