eb73192ec82d6dc9da325366d57305189a9cda52
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-02-01 20:06:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.cpp
+++ Difficulty: Basic/Peak element/peak-element.cpp
@@ -32,6 +32,50 @@ class Solution {
     }
 };
 
+////
+
+/*
+    int peakElement(vector<int> &arr) {
+        // Your code here
+        int n=arr.size();
+        int left = 0, right = n-1;
+        
+        while(left < right){
+            int mid = (left + right)/2;
+            
+            if(arr[mid] > arr[mid+1]){
+                right = mid;
+            }
+            else 
+                left = mid+1;
+        }
+        return left;
+    }
+*/
+
+//////Above is in O(logn)  tc. /////
+
+
+///////// IN O(n) tc //////
+/*
+    int peakElement(vector<int> &arr) {
+        // Your code here
+        int n=arr.size();
+        if(n==1) return 0;
+        
+        if(arr[0] > arr[1]) return 0;
+        
+        if (arr[n-1] > arr[n-2]) return n-1; 
+        
+        for(int i=1; i<n-1; i++){
+            if(arr[i-1] < arr[i] && arr[i] > arr[i+1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+*/
+
 //{ Driver Code Starts.
 
 int main() {
